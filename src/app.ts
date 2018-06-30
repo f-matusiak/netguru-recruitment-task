886998c542ae46a9eab64116bdaf5a85de5530f5
GiFree <fra.matusiak@gmail.com>
2018-07-01 00:22:38 +0200

    added possibility to sync database

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as db from './models';
 
 const app = express();
 
@@ -15,6 +16,16 @@ app.get('/movies', getMovies);
 import { getComments, postComments } from './routes/comments';
 app.post('/comments', postComments);
 app.get('/comments', getComments);
+
+app.post('/sync', (req, res, next) => {
+  db.syncComments()
+    .then(() => {
+      db.syncMovies()
+        .then(() => {
+          res.status(200).send({ message: 'sucessfully synced database!' });
+        });
+    });
+});
 
 app.get('*', (req, res, next) => {
   res.status(404).send('Error: Not Found');
